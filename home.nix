3307d29d329dcfb52e8aacca6743a9fa16c3031d
janusz-bit <janusz-bit@proton.me>
2025-11-29 22:57:21 +0100

    Added bootdev-cli

--- home.nix
+++ home.nix
@@ -137,6 +137,8 @@
 
     kdePackages.breeze # styl wyświetlania
 
+    bootdev-cli
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
